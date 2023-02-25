9a57cfe17c31de61dee23db9ccf8157a458aec96
samssjubair <samssjubair@gmail.com>
2023-02-25 17:58:41 +0600

    ts error ignore

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  ignoreBuildErrors: true,
   images: {
     domains: ['images.unsplash.com','hydeparkwinterwonderland.com','wembleypark.com','source.unsplash.com']
   }
