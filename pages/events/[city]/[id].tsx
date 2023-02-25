cb18af004961e2167be41856e395bcbc55cc92e8
samssjubair <samssjubair@gmail.com>
2023-02-25 15:37:51 +0600

    deployment error 5

--- pages/events/[city]/[id].tsx
+++ pages/events/[city]/[id].tsx
@@ -1,7 +1,19 @@
 import Image from 'next/image';
 import React from 'react';
 
-const CityEvents = ({data}) => {
+interface EventData {
+    id: string;
+    title: string;
+    city: string;
+    description: string;
+    image: string;
+  }
+  
+  interface CityEventsProps {
+    data: EventData;
+  }  
+
+const CityEvents: React.FC<CityEventsProps>  = ({data}) => {
     return (
         <div className="bg-white shadow-lg rounded-lg overflow-hidden">
         <Image width={400} height={400} src={data.image} alt="Event Image" className="h-2/6 w-full object-cover"/>
@@ -35,7 +47,7 @@ export async function getStaticPaths(){
 
 }
 
-export async function getStaticProps(context){
+export async function getStaticProps(context: any){
     console.log(context);
     const {allEvents}=await import('data/data.json');
     const data= allEvents.find(ev=>ev.id===context.params.id)
